1cca1f329c8d10150176f7b5431902963a1e3e90
patpatfc <bayraktarbatuhan2002@gmail.com>
2019-03-10 21:22:32 +0300

    cleaned repo

--- Pomelo_Main.py
+++ Pomelo_Main.py
@@ -3,85 +3,165 @@ import cv2.aruco as aruco
 from time import sleep
 from pololu_drv8835_rpi import motors
 import threading
-#sudo modprobe bcm2835-v4l2 //this makes picamera visible
+import RPi.GPIO as GPIO
+import PiWarsTurkiyeRobotKiti2019
+from PiWarsTurkiyeRobotKiti2019 import HizlandirilmisPiKamera
+import Pomelo_Monitor
+# sudo modprobe bcm2835-v4l2 //this makes picamera visible
 
-commands = [] #Commands array that we will execute
 
-#Setup function to define stuff
+motors = PiWarsTurkiyeRobotKiti2019.MotorKontrol ()
+controller = PiWarsTurkiyeRobotKiti2019.Kumanda ()
+controller.dinlemeyeBasla ()
+xy = controller.solVerileriOku ()
+xz = controller.sagVerileriOku ()
+xx = controller.butonlariOku ()
+commands = []
+
 def setup():
-    global cap, aruco_dict, parameters
-    cap = cv2.VideoCapture(-1)
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
-    parameters = aruco.DetectorParameters_create()
-    motors.setSpeeds(0, 0) #Stop motors
-    
-#Takes a picture with openCV
-def takePic():
-    global commands, cap, aruco_dict, parameters
-    ret, frame = cap.read()
-    ret, frame = cap.read() #Read twice to make sure the frame is the most current frame
-    
-    #If can't take picture, return
-    if not ret:
-        print("no pic")
-        return
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ids = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)[1]
+    global camera, aruco_dict, parameters
+    camera = HizlandirilmisPiKamera ()
+    camera.veriOkumayaBasla ()
+    aruco_dict = aruco.Dictionary_get (aruco.DICT_4X4_250)
+    parameters = aruco.DetectorParameters_create ()
+    motors.hizlariAyarla (0, 0)
+    GPIO.setmode (GPIO.BCM)
+    GPIO.setup (26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    print(GPIO.input (26))
 
-    #If no arucos found, return
-    if ids is None or len(ids) == 0:
+
+def forward(duration):
+    motors.hizlariAyarla (240, 240)  # max: 480
+    sleep (duration)
+    motors.hizlariAyarla (0, 0)
+
+
+def back(duration):
+    motors.hizlariAyarla (-240, -240)
+    sleep (duration)
+    motors.hizlariAyarla (0, 0)
+
+
+def right(duration):
+    motors.hizlariAyarla (-240, 240)
+    sleep (duration)
+    motors.hizlariAyarla (0, 0)
+
+
+def left(duration):
+    motors.hizlariAyarla (240, -240)
+    sleep (duration)
+    motors.hizlariAyarla (0, 0)
+
+
+def takePic():
+    global commands, camera, aruco_dict, parameters
+    frame = camera.veriOku ()
+
+    gray = cv2.cvtColor (frame, cv2.COLOR_BGR2GRAY)
+    
+
+    ids = aruco.detectMarkers (gray, aruco_dict, parameters=parameters)[1]
+
+    if ids is None or len (ids) == 0:
         print("no aruco")
         return
-    
-    #Prints the found arucos
-    ids = ids.tolist()
+
+    ids = ids.tolist ()
     commands = []
     for i in ids:
         commands += i
         print(i)
+    return commands
+    # if ids is not None and len(ids) > 0:print(ids.tolist()) #0, 18, 20, 39
 
-#Runs the commands (Note: If called more than one times in a row, it will execute the same commands until takePic() is called again.)
-def run():
-    global commands
-    n = 1 #Number of times to execute a function
-    for i in commands:
-        if i < 10:
-            n = i #First 9 blocks sets the n
-            
-        #More arucos can be added by checking different i values
-        if i == 11:
-            back(n)
-            n = 1
-        if i == 12:
-            forward(n)
-            n = 1
 
-#Takes input from console to take a picture or run the current commands
-def takeInput():
-    while(1):
-        try: n = int(input("1 - take pic\n2 - run\n"))
-        except: continue
-        if n == 1:
-            takePic()
-        if n == 2:
-            run()
-
-#Put custom functions after this
-def forward(n):
-    motors.setSpeeds(240, 240) #Max Speed: 480
-    sleep(n)
-    motors.setSpeeds(0, 0)
-
-def back(n):
-    motors.setSpeeds(-240, -240)
-    sleep(n)
-    motors.setSpeeds(0, 0)
+def run(commands):
+    duration = 1
+    if len(commands) >=1:
+        print(len(commands))
+        for i in range(len(commands)):
+            print(i)
+            arucoNumber = commands[i] 
+            if len(commands)-1>i:
+                arucoNumberSecond = commands[i+1]
+            else:
+                arucoNumberSecond = 1
+                print("no specified seconds")
+            if arucoNumber == 12:
+                if arucoNumberSecond <= 10:
+                    duration = arucoNumberSecond 
+                    print ("back", duration)
+                back (duration)  
+            if arucoNumber == 11:
+                if arucoNumberSecond <= 10:
+                    duration = arucoNumberSecond  
+                    print ("forward", duration)
+                forward (duration)
+            if arucoNumber == 14:
+                if arucoNumberSecond <= 10:
+                    duration = arucoNumberSecond  
+                left (duration)
+            if arucoNumber == 13:
+                if arucoNumberSecond <= 10:
+                    duration = arucoNumberSecond  
+                    print ("right", duration)
+                right (duration)
+            if arucoNumber == 15: duration = 3
     
 
-setup()
-thread = threading.Thread(target=takeInput) #Takes input from console in another thread
-thread.start() #Starts the thread
-while(1):
-    ret, frame = cap.read() #Refresh frames
-    sleep(0.05) #Tiny delay
+
+def takeInput():
+    while (1):
+        y = GPIO.input (26)
+        if (y == True):
+            print("button pressed")
+            commands = takePic ()
+            run (commands)
+            
+        while (xx != controller.butonlariOku () or xy != controller.sagVerileriOku () or xz != controller.solVerileriOku ()):
+            lx, ly = controller.solVerileriOku ()
+            print(lx, ly)
+            rightSpeed, leftSpeed = motors.kumandaVerisiniMotorVerilerineCevirme (lx, ly)
+            #leftSpeed = motors.kumandaVerisiniMotorVerilerineCevirme (lx, ly, False)
+            print(lx, " ", ly, " ", rightSpeed, " ", leftSpeed)
+            motors.hizlariAyarla (rightSpeed, leftSpeed)
+            sleep (0.3)
+            motors.hizlariAyarla (0, 0)
+
+        sleep (0.2)
+
+LCD = Monitor()
+
+def Monitor_loop():
+    LCD.Clear_Screen()
+    #camera must detect emotion
+    # search emotion in list
+    #emotion_number is = to emotions number in Emotions list
+    #LCD.ongoing_emotion = 1
+    LCD.check_emotion(LCD.ongoing_emotion, LCD.emotion_number, LCD.emotion_count)
+    LCD.Update_Screen()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            LCD.Exit_Screen()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e: #if e key is pressed
+                LCD.Enter_FullScreen()
+            if event.key == pygame.K_x: #if x key is pressed
+                LCD.Exit_FullScreen()
+            if event.key == pygame.K_q: #if q key is pressed
+                LCD.Exit_Screen()
+
+
+thread = threading.Thread (target=Monitor_loop)
+
+
+setup ()
+sleep (1)
+thread = threading.Thread (target=takeInput)
+thread.start ()
+
+while (1):
+    
+    frame = camera.veriOku ()
+    camera.kareyiGoster()
--- piwars.py
+++
@@ -1,8 +0,0 @@
-
-from PiWarsTurkiyeRobotKiti2019 import HizlandirilmisPiKamera
-from time import sleep
-
-camera = HizlandirilmisPiKamera()
-camera.veriOkumayaBasla()
-sleep(1)
-camera.kareyiGoster()
