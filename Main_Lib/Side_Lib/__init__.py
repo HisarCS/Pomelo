6a229ce71032293ddbeee3dfab7c63b4048e1e02
patpatfc <batuhan.bayraktar@ggl.hisarschool.k12.tr>
2019-09-13 12:04:26 +0300

    Added missing classes

---
+++ Main_Lib/Side_Lib/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = "1.1.0"
+
+from .Kumanda import Kumanda
+from .MotorKontrol import MotorKontrol
+from .HizlandirilmisPiKamera import HizlandirilmisPiKamera
---
+++ Main_Lib/__init__.py
@@ -0,0 +1,5 @@
+
+__version__ = "1.1.0"
+
+from .ArucoClass import Aruco
+from .MonitorClass import Monitor
