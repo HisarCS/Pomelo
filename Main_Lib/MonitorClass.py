8f44500f5b50b634ac83c98e439ff396bf814b85
AndyEmre <55792307+AndyEmreHis@users.noreply.github.com>
2019-12-31 21:49:45 +0300

    Update MonitorClass.py

--- Main_Lib/MonitorClass.py
+++ Main_Lib/MonitorClass.py
@@ -1,103 +1,102 @@
-#import pygame
-import sys
+import pygame
+import RPi.GPIO as GPIO
 from time import sleep
-#import cv2
-class Monitor:
+from ArucoClass import Aruco
+import threading
+import value
 
-    """def __init__(self):
+#c = threading.Condition()
+#detectedMarkers = None
+class Monitor(Aruco):
+    def __init__(self):
         self.white=(255,255,255)
         self.screen = pygame.display.set_mode ((480, 272), )
 
-        #monitor setup
+                #monitor setup
         pygame.init ()
         pygame.display.set_caption("Pomelo")
 
-        #emotion setup
-        self.Emotions = []
         self.Neutral_Happy2 =[]
         self.Blink = []
-        self.Emotions.append(self.Neutral_Happy2)
-        self.Emotions.append(self.Blink)
-        self.emotion_number = 1
-        self.emotion_count = 0
-        self.ongoing_emotion = 1
-        self.Blinks()
+        self.Angry = []
+
+        self.ongoing = 0
+
         self.Happys()
+        self.Angrys()
+        self.Blinks()
         
 
     def Happys(self):
         for x in range (37):
-            self.Neutral_Happy2.append(pygame.transform.scale(pygame.image.load('../Pomelo_Face/Neutral-Happy2/Pomelo_Eyes_Neutral-Happy2.'+str(x)+'.jpeg') , (480,272)))
-
+            self.Neutral_Happy2.append(pygame.transform.scale(pygame.image.load('/home/pi/pomelo/Pomelo_Face/Neutral-Happy2/Pomelo_Eyes_Neutral-Happy2.'+str(x)+'.jpeg') , (480,272)))
+            
     def Blinks(self):
         for x in range (37):
-            self.Blink.append(pygame.transform.scale(pygame.image.load('../Pomelo_Face/Blink/Pomelo_Eyes_Blink.'+str(x)+'.jpeg'), (480, 272)))
+            self.Blink.append(pygame.transform.scale(pygame.image.load('/home/pi/pomelo/Pomelo_Face/Blink/Pomelo_Eyes_Blink.'+str(x)+'.jpeg'), (480, 272)))
 
-    #setup finished
 
-    #check if an emotion is displayed
-    def check_emotion(self, ongoing_emotion, emotion_number, emotion_count):
-        if ongoing_emotion == 1:
-            self.Blit_Face(emotion_number, emotion_count)
-            self.emotion_count += 1
-        if ongoing_emotion == 0:
-            self.No_Emotion()
+    def Angrys(self):
+        for x in range (37):
+            self.Angry.append(pygame.transform.scale(pygame.image.load('/home/pi/pomelo/Pomelo_Face/Angry/Pomelo_Eyes_Neutral_Angry.'+str(x)+'.jpeg'), (480,272)))
 
-    # when no emotion is displayed
-    def No_Emotion(self):
-        self.screen.blit (self.Emotions[0][0], (0, 0))
+    def AngryBlit(self):
+        self.ongoing = 1
+        for x in range(37):
+            self.screen.blit(self.Angry[x], (0,0))
+            pygame.display.update()
+        self.ongoing = 0
+        sleep(0.2)
 
-    # display emotion
-    def Blit_Face(self, emotion_number, emotion_count):
-        self.screen.blit (self.Emotions[emotion_number][emotion_count], (0, 0))
-        self.Pause(0.00001)
-        if self.emotion_count == 36:
-            self.emotion_count = -1
-            self.ongoing_emotion = 1
-        else:
-            self.ongoing_emotion = 1
+                         
+    def HappyBlit(self):
+        self.ongoing = 1
+        for x in range(37):
+            self.screen.blit(self.Neutral_Happy2[x], (0,0))
+            pygame.display.update()
+        self.ongoing = 0
+        sleep(0.2)
+                         
+    def BlinkBlit(self):
+        self.ongoing = 1
+        for x in range(37):
+            self.screen.blit(self.Blink[x], (0,0))
+            pygame.display.update()
+            sleep(0.2)
+        self.ongoing = 0
 
-    #fill screen white
-    def Clear_Screen(self):
-        self.screen.fill (self.white)
 
-    def Update_Screen(self):
-        pygame.display.update ()
-
-    #enter fullscreen
-    def Enter_FullScreen(self):
-        self.screen = pygame.display.set_mode ((480, 272), pygame.FULLSCREEN)
-
-    #exit fullscreen
-    def Exit_FullScreen(self):
-        self.screen = pygame.display.set_mode ((480, 272), )
-
-    #end pygame
-    def Exit_Screen(self):
-        pygame.quit ()
-        sys.exit ()
-
-    #code pauses for the given amount of seconds
-    def Pause(self, duration):
-        sleep(duration)"""
-
-    def Loop(self):
+    def Loop(self, q):    
         while True:
-            #self.Clear_Screen()
-            print("1")
-            #camera must detect emotion
-            # search emotion in list
-            #emotion_number is = to emotions number in Emotions list
-            #self.ongoing_emotion = 1
-            """self.check_emotion(self.ongoing_emotion, self.emotion_number, self.emotion_count)
-            self.Update_Screen()
+            print('1')
+            #if ongoing == 0:
+            self.screen.blit(pygame.transform.scale(pygame.image.load('/home/pi/pomelo/Pomelo_Face/Neutral-Happy2/Pomelo_Eyes_Neutral-Happy2.'+'0'+'.jpeg'), (480,272)), (0,0))
+            
+            val = q.get()
+            
+            if val == 0:
+                self.AngryBlit()
+                self.screen.blit(pygame.transform.scale(pygame.image.load('/home/pi/pomelo/Pomelo_Face/Neutral-Happy2/Pomelo_Eyes_Neutral-Happy2.'+'0'+'.jpeg'), (480,272)), (0,0))
+                print("not found")
+                pygame.display.update()
+            elif val == 1:
+                self.HappyBlit()
+                self.screen.blit(pygame.transform.scale(pygame.image.load('/home/pi/pomelo/Pomelo_Face/Neutral-Happy2/Pomelo_Eyes_Neutral-Happy2.'+'0'+'.jpeg'), (480,272)), (0,0))
+                pygame.display.update()
+            
+            pygame.display.update()
+            
+            #self.aruco = super(Monitor,self).takeMonitorInput()
+
+            #self.BlinkBlit()
+                
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.Exit_Screen()
+                    pygame.display.quit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_e: #if e key is pressed
-                        self.Enter_FullScreen()
-                    if event.key == pygame.K_x: #if x key is pressed
-                        self.Exit_FullScreen()
-                    if event.key == pygame.K_q: #if q key is pressed
-                        self.Exit_Screen()"""
+                    if event.key == pygame.K_h: #if e key is pressed
+                        self.HappyBlit()
+                        print("Hap")
+                    if event.key == pygame.K_a: #if x key is pressed
+                        self.AngryBlit()
+                        print("Ang")
